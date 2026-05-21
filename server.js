a2e8bab662ff6ce622a29e485c12e777fe8ebb44
Ubuntu <azureuser@azureuser.blkoif13hp3e5kyupzkm1ttmlh.rx.internal.cloudapp.net>
2026-05-21 11:53:01 +0000

    Added CI/CD pipeline

--- server.js
+++ server.js
@@ -1,42 +1,26 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
 
-// create express app
-var app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
 
-// parse application/json
-app.use(bodyParser.json())
 
-// Configuring the database
-var dbConfig = require('./config/database.config.js');
-var mongoose = require('mongoose');
+// Simple route
 
-mongoose.Promise = global.Promise;
+app.get('/', (req, res) => {
 
-mongoose.connect(dbConfig.url, {
-	useMongoClient: true
+    res.json({
+
+        message: "Azure VM + Nginx + CI/CD project working successfully"
+
+    });
+
 });
 
-mongoose.connection.on('error', function() {
-    console.log('Could not connect to the database. Exiting now...');
-    process.exit();
-});
-mongoose.connection.once('open', function() {
-    console.log("Successfully connected to the database");
-})
 
-// define a simple route
-app.get('/', function(req, res){
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
-});
+// Start server
 
-require('./app/routes/note.routes.js')(app);
-require('./app/routes/location.routes.js')(app);
+app.listen(3000, () => {
 
-// listen for requests
-app.listen(3000, function(){
     console.log("Server is listening on port 3000");
+
 });
